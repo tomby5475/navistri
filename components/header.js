bc369defab284285c99441ef18915d466304d4ec
tomby5475 <pushkareva.nataly@gmail.com>
2024-09-05 14:40:02 +0200

    create ligin button

--- components/header.js
+++ components/header.js
@@ -1,21 +1,24 @@
 import Image from "next/image"
 import logo from '../public/logo.png'
 import arrow from '../public/images/glyph.png'
+import styles from '../styles/Home.module.css'
 
 export default function Header () {
     return (
-        <div className="nav">
+        <div className={styles.nav}>
             <Image
                 src={logo}
                 alt="logo"
             />
-            <button> 
-                LOGIN 
-                <Image 
-                    src={arrow} 
-                    alt="arrow" 
-                />
-            </button>
+            <div className={styles.login_btn_wrapper}>
+                <button className={styles.login_btn}> 
+                    LOGIN 
+                    <Image className={styles.login_arrow}
+                        src={arrow} 
+                        alt="arrow" 
+                    />
+                </button>
+            </div>
         </div>
     )
 }

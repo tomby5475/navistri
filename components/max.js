83b6a0ea2e6c7e3218180f3e46292be2d0de7ae1
tomby5475 <pushkareva.nataly@gmail.com>
2024-09-24 14:00:12 +0200

    add 1-st tutor description

--- components/max.js
+++ components/max.js
@@ -4,11 +4,21 @@ import max from '../public/images/max@2x.png'
 
 export default function Max () {
     return (
-        <div className={styles.max}>
-           <Image className={styles.max_img}
+        <div className={styles.tutor}>
+           <Image className={styles.tutor_img}
                 src={max}
                 alt="Max's picture"
             />
+            <article className={styles.tutor_about}>
+                <h2>Impactful Storytelling</h2>
+                <p>In the age of wars, misinformation, and climate change, unveiling the truth through effective narratives is more important than ever. At NAVISTRI, I share insights gleaned from our work in the Ukrainian PR Army. </p>
+                <hr/>
+                <strong>Max Souschuk</strong>
+                <div className={styles.socialBtns}>
+                    <a href="https://www.linkedin.com/in/sushchuk">LinkedIn</a>
+                    <a href="https://bsky.app/profile/sushchuk.bsky.social">BlueSky</a>
+                </div>
+            </article>
         </div>
     )
 }
